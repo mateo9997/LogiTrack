9d7a5d2b9b94ab0cc33ded9ec23819388f65b5c3
mateo9997 <mj@mateoramon.com>
2024-12-29 11:20:08 +0100

    not complete code(change of working station)

---
+++ backend/src/Entity/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\OrderRepository;
+
+
+class Order
+{
+
+}
---
+++ backend/src/Repository/OrderRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Order;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class OrderRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Order::class);
+    }
+    public function findByFilters(?string $status, ?string $assignedUser):
+    {
+        $qb = $this->createQueryBuilder('o');
+
+        if ($status) {
+            $qb->andWwhere
+        }
+    }
+}
