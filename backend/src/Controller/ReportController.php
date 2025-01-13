79ad1f0686563a64d3f3f20776f2e6f7c7e29d65
mateo9997 <mj@mateoramon.com>
2025-01-13 09:40:28 +0100

    added report controller for backend

---
+++ backend/src/Controller/ReportController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\ReportService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/reports')]
+class ReportController extends AbstractController
+{
+    public function __construct(private ReportService $reportService) {}
+
+    /**
+     * Return an overview of total orders, status counts.
+     */
+    #[Route('/overview', name: 'report_overview', methods: ['GET'])]
+    #[IsGranted("ROLE_COORDINATOR")]
+    public function overview(): JsonResponse
+    {
+        $reportData = $this->reportService->getOrderOverview();
+        return $this->json($reportData, Response::HTTP_OK);
+    }
+
+    /**
+     * Return fulfillment rates (delivered vs total).
+     */
+    #[route('/fulfillment', name: 'report_fulfillment', methods: ['GET'])]
+    #[IsGranted("ROLE_COORDINATOR")]
+    public function fulfillmentRates(): JsonResponse
+    {
+        $data = $this->reportService->getFulfillmentRates();
+        return $this->json($data, Response::HTTP_OK);
+    }
+
+
+}
